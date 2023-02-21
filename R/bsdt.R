9978f4a54d0de6211d61167c9538318e34adcc9c
Nikolaus Umlauf <Nikolaus.Umlauf@uibk.ac.at>
2023-02-21 15:34:41 +0100

    Updating the man model fitting function.

--- R/bsdt.R
+++ R/bsdt.R
@@ -1,15 +1,16 @@
 ## Soft Distributional Regression Tree Machine.
 bsdt <- function(..., k = 4, maxit = 100, batch_ids = NULL,
-  nu = 0.1, lambda = 0.0001, omin = 50, faster = TRUE,
+  nu = 0.1, lambda = 0.0001, omin = 50, faster = TRUE, shadow = FALSE,
   plot = TRUE, verbose = TRUE, flush = TRUE, eps_logLik = Inf)
 {
   mf <- srt(..., ret.mfd = TRUE)
+
   if(nrow(mf$X[[1]]) < omin)
     stop("argument omin is too large!")
   fit <- .bsdt.fit(mf$X, mf$y, family = mf$family, offset = mf$offset,
     k = k, maxit = maxit, batch_ids = batch_ids, faster = faster,
     nu = nu, plot = plot, verbose = verbose, flush = flush, lambda = lambda,
-    omin = omin, eps_logLik = eps_logLik)
+    omin = omin, eps_logLik = eps_logLik, shadow = shadow)
   mf[names(fit)] <- fit
   mf$call <- match.call()
   class(mf) <- c("bsdt", "srt")
@@ -27,7 +28,7 @@ fmt <- Vectorize(function(x, width = 8, digits = 2) {
 })
 
 .bsdt.fit <- function(X, y, family, k, maxit, batch_ids, nu, start, offset,
-  plot, verbose, flush, digits = 4L, lambda, faster, omin, eps_logLik)
+  plot, verbose, flush, digits = 4L, lambda, faster, omin, eps_logLik, shadow)
 {
   nx <- names(X)
   N <- nrow(X[[1L]])
@@ -96,28 +97,36 @@ fmt <- Vectorize(function(x, width = 8, digits = 2) {
     for(i in nx) {
       ttrees[[i]] <- .fit.stree(x = X[[i]][batch_ids[[iter]], , drop = FALSE],
         y = yi, eta = eta, family = family, i = i, k = k, lambda = lambda[i],
-        faster = faster, omin = omin)
+        faster = faster, omin = omin, shadow = shadow)
       eta[[i]] <- eta[[i]] + nu * predict(ttrees[[i]],
         data = X[[i]][batch_ids[[iter]], , drop = FALSE])
     }
 
     ll1 <- family$loglik(yi, family$map2par(eta))
     eps <- abs((ll1 - ll0) / ll0)
+    if(m > 1)
+      eps2 <- abs((ll0 - ll_save[length(ll_save)]) / ll_save[length(ll_save)])
+    else
+      eps2 <- eps
 
-    if((ll1 > ll0) & (eps < eps_logLik)) {
+    if((ll1 > ll0) & (eps < eps_logLik) & (eps2 < 0.9)) {
       ll_save <- c(ll_save, ll1)
 
       for(i in nx) {
         trees[[i]][[m]] <- ttrees[[i]]
       }
       m <- m + 1L
+    } else {
+#      cat("\n---\n")
+#      print(c("ll0" = ll0, "ll1" = ll1, "eps" = eps, "eps2" = eps2))
+#      cat("---\n")
     }
 
     if(plot & (m > 1)) {
       par(mfrow = c(2, 2), mar = c(4.5, 4.5, 1.1, 1.1))
       plot(ll_save, type = "l", lwd = 2, xlab = "#Trees", ylab = "logLik")
       par <- family$map2par(eta)
-      if(!is.null(family$p)) {
+      if(!is.null(family[["p"]])) {
         if(discrete) {
           ymin <- min(yi, na.rm = TRUE)
           a <- family$p(ifelse(yi == ymin, yi, yi - 1), par)
@@ -165,8 +174,10 @@ fmt <- Vectorize(function(x, width = 8, digits = 2) {
     if(verbose) {
       cat(if(ia & flush) "\r" else if(iter > 1) "\n" else NULL)
       cat("logLik = ", fmt(ll_save[length(ll_save)], width = 8, digits = digits),
-        " eps = ", fmt(eps, width = 6, digits = digits + 2),
-        " iter = ", iter,
+        " eps1 = ", fmt(eps, width = 6, digits = digits + 2),
+        " eps2 = ", fmt(eps2, width = 6, digits = digits + 2),
+        " ntrees = ", fmt(length(ll_save), width = nchar(as.character(maxit)), digits = 0),
+        " iter = ", fmt(iter, width = nchar(as.character(maxit)), digits = 0),
         if(!ia) "\n" else NULL, sep = "")
     }
   }
@@ -191,9 +202,104 @@ fmt <- Vectorize(function(x, width = 8, digits = 2) {
   return(rval)
 }
 
-.fit.stree <- function(x, y, eta, family, i, k, lambda, faster, omin)
+fit_split <- function(x, y, W, eps = 0.001, lambda = 0.01, nu = 0.001, maxit = 100)
+{
+  n <- length(y)
+  eta <- rep(0, n)
+  f1 <- f2 <- rep(0, n)
+  par <- rep(1, 3)
+
+  g <- function(x) {
+    1/(1 + exp2(-x))
+  }
+
+  eps0 <- eps + 1
+  k <- 1
+  while(eps < eps0) {
+    eta0 <- eta
+    eta <- eta - f1
+    z <- y - eta
+    b2 <- mean(z * W)
+    f1 <- rep(b2, n)
+    eta <- eta + f1
+
+    eta <- eta - f2
+    z <- y - eta
+
+    r <- z - f2
+
+    xw <- par[2] + par[3] * x
+
+    J <- -1 * cbind(
+      1/(1 + exp2(-xw)), 
+      par[1] * exp2(-xw)/(1 + exp2(-xw))^2,
+      par[1] * (exp2(-xw) * x)/(1 + exp2(-xw))^2
+    )
+
+    delta <- solve(crossprod(J * W, J) + diag(lambda, 3)) %*% t(J * W) %*% r
+
+    par <- par - nu * delta
+
+    f2 <- par[1] *  g(par[2] + par[3] * x)
+
+    eta <- eta + f2
+
+    eps0 <- mean(abs((eta - eta0)/eta0))
+
+    k <- k + 1
+
+    if(k >= maxit)
+      break
+  }
+
+  rval <- list(
+    "parameters" = c(b2 + par[1], b2, par[2], par[3]),
+    "fitted.values" = eta,
+    "iterations" = k
+  )
+
+  return(rval)
+}
+
+optim_split <- function(x, y, W, family, eta, id, Y, lambda)
+{
+  fs <- fit_split(x, y, W, lambda = lambda)
+  eta[[id]] <- eta[[id]] + fitted(fs)
+  rval <- list(
+    "par" = fs$parameters,
+    "value" = -1 * family$loglik(Y, family$map2par(eta))
+  )
+  return(rval)
+}
+
+.fit.stree <- function(x, y, eta, family, i, k, lambda, faster, omin, shadow)
 {
   coef <- list()
+
+  nshadow <- ncol(x) - 1L
+  if(!is.logical(shadow)) {
+    if(shadow > 0) {
+      nshadow <- shadow
+      shadow <- TRUE
+    } else {
+      shadow <- FALSE
+    }
+  }
+
+  if(shadow) {
+    xs <- x[, -grep("(Intercept)", colnames(x)), drop = FALSE]
+    uxs <- apply(xs, 2, function(z) length(unique(z)))
+    if(!any(uxs > 10)) {
+      shadow <- FALSE
+    } else {
+      xs <- xs[, uxs > 10, drop = FALSE]
+      xs <- xs[, rep(1:ncol(xs), length.out = nshadow)]
+      for(j in 1:ncol(xs))
+        xs[, j] <- sample(xs[, j])
+      colnames(xs) <- paste0("SHADOW_", 1:ncol(xs))
+      x <- cbind(x, xs)
+    }
+  }
 
   if(ncol(x) > 1) {
     fn <- function(par, x, weights) {
@@ -205,6 +311,7 @@ fmt <- Vectorize(function(x, width = 8, digits = 2) {
       ll <- family$d(y, family$map2par(eta), log = TRUE)
       ll[(ll < -1000) | !is.finite(ll)] <- NA
       ll <- sum(ll, na.rm = TRUE)
+      ## ll <- mean(ll, na.rm = TRUE)
       ll <- ll - lambda * sum(w^2)
       return(-1 * ll)
     }
@@ -219,6 +326,7 @@ fmt <- Vectorize(function(x, width = 8, digits = 2) {
       score <- process_derivs(family$score[[i]](y, family$map2par(eta), id = i), is.weight = FALSE)
       s <- cbind(score * G, score * weights * (beta[1] - beta[2]) * sigmoid_deriv(xw) * x)
       grad <- colSums(s) - diag(c(0, 0, lambda, lambda)) %*% c(beta, w)
+      ## grad <- colMeans(s) - diag(c(0, 0, lambda, lambda)) %*% c(beta, w)
      -grad
     }
 
@@ -242,7 +350,7 @@ fmt <- Vectorize(function(x, width = 8, digits = 2) {
               xj <- x[, vn[j]]
               ok <- FALSE
               if((uxj <- length(unique(xj))) > 7) {
-                breaks <- unique(quantile(x[, vn[j]], prob = seq(0, 1, length = 5)))
+                breaks <- unique(quantile(x[, vn[j]], prob = seq(0, 1, length = 7)))
                 if(length(breaks) > 2L) {
                   xj <- cut(xj, breaks = breaks, include.lowest = TRUE)
                   ok <- TRUE
@@ -254,10 +362,12 @@ fmt <- Vectorize(function(x, width = 8, digits = 2) {
                 }
               }
               if(ok) {
-                mj <- lm(z ~ xj, weights = hess * N[, l])
-                fstat <- summary(mj)$fstatistic
-                if(!is.null(fstat))
-                  pval[j] <- pf(fstat[1L], fstat[2L], fstat[3L], lower.tail = FALSE)
+                mj <- try(lm(z ~ xj, weights = hess * N[, l]), silent = TRUE)
+                if(!inherits(mj, "try-error")) {
+                  fstat <- summary(mj)$fstatistic
+                  if(!is.null(fstat))
+                    pval[j] <- pf(fstat[1L], fstat[2L], fstat[3L], lower.tail = FALSE)
+                }
               }
             }
             if(!all(is.na(pval))) {
@@ -265,6 +375,10 @@ fmt <- Vectorize(function(x, width = 8, digits = 2) {
               val_j[[l]] <- optim(c(0.001, 0.001, 0.001, 1), fn = fn, gr = gr,
                 x = x[, c("(Intercept)", vn[j])], weights = N[, l], method = "L-BFGS-B",
                 control = list("pgtol"= 0.00001))
+
+#              val_j[[l]] <- optim_split(x = x[, vn[j]], y = z, W = hess * N[, l],
+#                family = family, eta = eta, id = i, Y = y, lambda = lambda)
+
               val_j[[l]]$index <- l
               val_j[[l]]$variable <- vn[j]
               val_j[[l]]$contrib <- -1 * val_j[[l]]$value - ll0
@@ -282,7 +396,7 @@ fmt <- Vectorize(function(x, width = 8, digits = 2) {
           val_l <- list()
           for(l in 1:ncol(N)) {
             if(sum(N[, l] > 0.0001) >= omin) {
-              val_l[[l]] <- optim(c(0.1, 0.1, 1, 1), fn = fn, gr = gr,
+              val_l[[l]] <- optim(c(0.001, 0.001, 0.001, 1), fn = fn, gr = gr,
                 x = x[, c("(Intercept)", j)], weights = N[, l], method = "L-BFGS-B",
                 control = list("pgtol"= 0.00001))
             } else {
@@ -292,11 +406,19 @@ fmt <- Vectorize(function(x, width = 8, digits = 2) {
           l <- which.min(sapply(val_l, function(x) x$value))
           val_j[[j]] <- val_l[[l]]
           val_j[[j]]$index <- l
-          val_j[[j]]$contrib <- -1 * val_j[[l]]$value - ll0
+          val_j[[j]]$contrib <- -1 * val_j[[j]]$value - ll0
         }
         j <- which.min(sapply(val_j, function(x) x$value))
         vj <- vn[j]
       }
+
+      if(length(vj)) {
+        if(grepl("SHADOW_", vj)) {
+          cat("\n.. .. ", i, ": selected shadow variable at depth ", K, "\n", sep = "")
+          break
+        }
+      }
+
       coef[[K]] <- list(
         "coefficients" = val_j[[j]]$par,
         "value" = -1 * val_j[[j]]$value,
@@ -344,18 +466,20 @@ fmt <- Vectorize(function(x, width = 8, digits = 2) {
 predict.stree <- function(object, data, ...)
 {
   fit <- rep(0, nrow(data))
-  if(length(object) < 2L) {
-    fit <- fit + object[[1]]$coefficients
-  } else {
-    N <- data.frame("r" = rep(1, nrow(data)))
-    for(j in 1:length(object)) {
-      beta <- object[[j]]$coefficients[1:2]
-      w <- object[[j]]$coefficients[-c(1:2)]
-      g <- sigmoid(data[, c("(Intercept)", object[[j]]$variable)] %*% w)
-      G <- cbind(g, 1 - g) * N[, object[[j]]$index]
-      fit <- fit + drop(G %*% beta)
-      N[, object[[j]]$index] <- G
-      N <- as.data.frame(as.matrix(N))
+  if(length(object) > 0) {
+    if((length(object) < 2L) & (length(object[[1]]$coefficients) < 2L)) {
+      fit <- fit + object[[1]]$coefficients
+    } else {
+      N <- data.frame("r" = rep(1, nrow(data)))
+      for(j in 1:length(object)) {
+        beta <- object[[j]]$coefficients[1:2]
+        w <- object[[j]]$coefficients[-c(1:2)]
+        g <- sigmoid(data[, c("(Intercept)", object[[j]]$variable)] %*% w)
+        G <- cbind(g, 1 - g) * N[, object[[j]]$index]
+        fit <- fit + drop(G %*% beta)
+        N[, object[[j]]$index] <- G
+        N <- as.data.frame(as.matrix(N))
+      }
     }
   }
   return(fit)
@@ -387,22 +511,25 @@ variables <- function(x)
 contrib <- function(x) {
   vars <- contrib <- NULL
   res <- list()
-  K <- 0
   if(length(x$trees)) {
     nx <- names(x$trees)
-    vars <- contrib <- list()
+    vars <- contrib <- iter <- depth <- list()
     for(i in nx) {
       for(j in 1:length(x$trees[[i]])) {
-        for(k in 1:length(x$trees[[i]][[j]])) {
-          vars[[i]] <- c(vars[[i]], x$trees[[i]][[j]][[k]]$variable)
-          cj <- x$trees[[i]][[j]][[k]]$contrib
-          if(is.null(cj))
-            cj <- 0
-          contrib[[i]] <- c(contrib[[i]], cj)
-          K <- K + 1
+        if(length(x$trees[[i]][[j]]) > 0) {
+          for(k in 1:length(x$trees[[i]][[j]])) {
+            vars[[i]] <- c(vars[[i]], x$trees[[i]][[j]][[k]]$variable)
+            cj <- x$trees[[i]][[j]][[k]]$contrib
+            if(is.null(cj))
+              cj <- 0
+            contrib[[i]] <- c(contrib[[i]], cj)
+            iter[[i]] <- c(iter[[i]], j)
+            depth[[i]] <- c(depth[[i]], k)
+          }
         }
       }
-      res[[i]] <- data.frame("variable" = vars[[i]], "contrib" = contrib[[i]])
+      res[[i]] <- data.frame("variable" = vars[[i]], "contrib" = contrib[[i]],
+        "iter" = iter[[i]], "depth" = depth[[i]])
     }
   }
 
@@ -411,53 +538,113 @@ contrib <- function(x) {
 }
 
 plot.bsdt_contrib <- function(x, model = NULL, legend = TRUE, ...) {
-  if(!is.null(model))
+  if(!is.null(model)) {
+    if(!is.character(model))
+      model <- names(x)[model]
     model <- names(x)[pmatch(model, names(x))]
-  else
+  } else {
     model <- names(x)
+  }
   x <- x[model]
-  res <- list()
-  for(i in names(x)) {
-    vn <- x[[i]]$variable
-    vc <- list()
-    for(j in vn) {
-      cd <- x[[i]][x[[i]]$variable == j, , drop = FALSE]
-      vc[[j]] <- sum(cd$contrib)
-    }
-    vc <- sort(unlist(vc), decreasing = TRUE)
-    res[[i]] <- vc
-  }
 
-  vn <- unique(unlist(lapply(res, names)))
-
-  res2 <- list()
-  for(j in vn) {
-    for(i in names(res)) {
-      res2[[j]] <- c(res2[[j]], res[[i]][j])
-    }
-    names(res2[[j]]) <- names(res)
-  }
-  res2 <- do.call("cbind", res2)
-  res2[is.na(res2)] <- 0
-
-  j <- apply(res2, 2, sum)
-  res2 <- res2 #/ max(j)
-  res2 <- res2[, order(j, decreasing = TRUE), drop = FALSE]
+  which <- list(...)$which
+  if(is.null(which))
+    which <- "barplot"
+  if(is.character(which))
+    which <- match.arg(which, c("barplot", "paths"))
+  else
+    which <- c("barplot", "paths")[which[1]]
 
   plot <- list(...)$plot
   if(is.null(plot))
     plot <- TRUE
 
+  if(which == "barplot") {
+    res <- list()
+    for(i in names(x)) {
+      vn <- x[[i]]$variable
+      vc <- list()
+      for(j in vn) {
+        cd <- x[[i]][x[[i]]$variable == j, , drop = FALSE]
+        vc[[j]] <- sum(cd$contrib)
+      }
+      vc <- sort(unlist(vc), decreasing = TRUE)
+      res[[i]] <- vc
+    }
+
+    vn <- unique(unlist(lapply(res, names)))
+
+    res2 <- list()
+    for(j in vn) {
+      for(i in names(res)) {
+        res2[[j]] <- c(res2[[j]], res[[i]][j])
+      }
+      names(res2[[j]]) <- names(res)
+    }
+    res2 <- do.call("cbind", res2)
+    res2[is.na(res2)] <- 0
+
+    j <- apply(res2, 2, sum)
+    res2 <- res2 #/ max(j)
+    res2 <- res2[, order(j, decreasing = TRUE), drop = FALSE]
+
+    if(isTRUE(plot)) {
+      rsum <- apply(res2, 1, sum, na.rm = TRUE)
+      res3 <- res2
+      for(j in names(rsum))
+        res3[j, ] <- res3[j, ] / rsum[j]
+      j <- apply(res3, 2, sum)
+      res3 <- res3[, order(j, decreasing = TRUE), drop = FALSE]
+
+      N <- nrow(res2)
+      col <- if(N < 2) "lightgray" else colorspace::rainbow_hcl(N)
+
+      ylab <- list(...)$ylab
+      if(is.null(ylab))
+        ylab <- "logLik contribution % / Parameter"
+
+      plt <- barplot(res3 * 100, col = col, ylab = ylab,
+          xaxt = "n")
+
+      text(plt, par("usr")[3], labels = colnames(res3),
+        srt = 50, adj = c(1.1, 1.1), xpd = TRUE, cex = 1)
+
+    }
+  }
+
+  if(which == "paths") {
+    res <- list()
+    for(i in names(x)) {
+      vn <- x[[i]]$variable
+      vc <- list()
+      maxit <- max(x[[i]]$iter)
+      for(j in vn) {
+        cp <- rep(0, maxit)
+        cd <- x[[i]][x[[i]]$variable == j, , drop = FALSE]
+        for(l in unique(cd$iter)) {
+          cp[l] <- sum(cd$contrib[cd$iter == l])
+        }
+        vc[[j]] <- cumsum(cp)
+      }
+      vc <- do.call("cbind", vc)
+      res[[i]] <- vc
+      N <- -1L
+      res2 <- NULL
+    }
+
+    ylim <- range(sapply(res, range))
+    xlim <- c(1, max(sapply(res, nrow)))
+
+    plot(1, 1, xlim = xlim, ylim = ylim, type = "n",
+      xlab = "Iteration", ylab = "logLik contribution")
+
+    col <- colorspace::rainbow_hcl(length(res))
+
+    for(i in names(res))
+      matplot(res[[i]], type = "l", lty = 1, col = col, add = TRUE)
+  }
+
   if(isTRUE(plot)) {
-    N <- nrow(res2)
-    col <- if(N < 2) "lightgray" else colorspace::rainbow_hcl(N)
-
-    plt <- barplot(res2, col = col, ylab = "logLik contribution",
-        xaxt = "n")
-
-    text(plt, par("usr")[3], labels = colnames(res2),
-      srt = 50, adj = c(1.1, 1.1), xpd = TRUE, cex = 1)
-
     if(!isFALSE(legend) & (N > 1)) {
       pos <- list(...)$pos
       if(is.null(pos))
@@ -475,6 +662,57 @@ plot.bsdt_contrib <- function(x, model = NULL, legend = TRUE, ...) {
   }
 
   invisible(res2)
+}
+
+new_formula <- function(x, thres = 0.01) {
+  ff <- formula(x)
+  rn <- as.character(ff[[1]][2])
+  if(is.null(x$model))
+    x$model <- model.frame(x)
+  dc <- lapply(x$model, function(x) attr(terms(x), "dataClasses"))
+  fv <- list()
+  for(j in names(dc)) {
+    fv[[j]] <- names(dc[[j]])[dc[[j]] == "factor"]
+  }
+  x <- contrib(x)
+  res <- plot.bsdt_contrib(x, plot = FALSE)
+  rsum <- apply(res, 1, sum, na.rm = TRUE)
+  for(j in names(rsum))
+    res[j, ] <- res[j, ] / rsum[j]
+  res[res < thres] <- NA
+  vn <- colnames(res)
+  f <- list()
+  for(i in rownames(res)) {
+    f[[i]] <- c(f[[i]], vn[!is.na(res[i, ])])
+    if(length(f[[i]]) < 1L)
+      f[[i]] <- "1"
+  }
+  for(j in 1:length(f)) {
+    if(length(fv[[j]]) > 0) {
+      for(i in fv[[j]]) {
+        if(length(l <- grepl(i, f[[j]], fixed = TRUE)))
+          f[[j]][l] <- i
+      }
+    }
+    f[[j]] <- sort(unique(f[[j]]))
+  }
+  f[[1]] <- as.formula(paste(rn, "~", paste(f[[1]], collapse = "+")),
+    env = .GlobalEnv)
+  if(length(f) > 1) {
+    for(j in 2:length(f))
+      f[[j]] <- as.formula(paste("~", paste(f[[j]], collapse = "+")), env = .GlobalEnv)
+  }
+
+  drop <- list()
+  for(j in names(f)) {
+    av1 <- unique(all.vars(ff[[j]]))
+    av2 <- unique(all.vars(f[[j]]))
+    drop[[j]] <- av1[!(av1 %in% av2)]
+  }
+
+  attr(f, "dropped") <- drop
+
+  f
 }
 
 plot.bsdt_variables <- function(x, legend = TRUE, ...)
